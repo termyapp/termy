406f584f466ce7ffffdcad0a1dc6923ccede0cef
martonlanga <martonlanga@gmail.com>
2020-09-17 20:22:38 +0200

    Fix SSR

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,10 +1,10 @@
 import { AppProps } from 'next/app'
 import React from 'react'
-import { reset } from 'stitches-reset'
-import { css } from '../stitches.config'
-import '../styles/index.css'
+// import { globalStyles } from '../stitches.config'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  // globalStyles()
+
   return <Component {...pageProps} />
 }
 
