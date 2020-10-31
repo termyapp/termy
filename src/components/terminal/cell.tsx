90521135862d0c4acae909a3097693c576218e2c
martonlanga <martonlanga@gmail.com>
2020-10-31 20:45:13 +0100

    Implement cd funtion

--- src/components/terminal/cell.tsx
+++ src/components/terminal/cell.tsx
@@ -9,6 +9,7 @@ import {
 } from '../../../types'
 import { ipc, useListener } from '../../lib'
 import { styled } from '../../stitches.config'
+import useStore from '../../store'
 import List from '../custom/list'
 import Prompt from './prompt'
 
@@ -56,13 +57,29 @@ const Container = styled('div', {
 })
 
 const ApiRenderer: React.FC<CellType> = ({ id, currentDir, input }) => {
+  const { setCurrentDir } = useStore()
+  const [output, setOutput] = useState('')
+
+  useListener(
+    'data',
+    id,
+    (message: ServerDataMessage) => {
+      const data = JSON.parse(message.data)
+      if (data.cd) {
+        setCurrentDir(id, data.cd)
+      }
+      setOutput(data.output)
+    },
+    [],
+  )
+
   switch (input) {
     // check if custom renderer is available
     case 'list':
       return <List path={currentDir} />
     default:
-      // default to auto renderer (generate html)
-      return <div>Auto Renderer</div>
+      // default to auto renderer (markdown?)
+      return <div>{output}</div>
   }
 }
 
