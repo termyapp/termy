1506ac1e6b381b5f2f4177ed6c646fdcbcde7b12
martonlanga <martonlanga@gmail.com>
2020-11-08 16:00:44 +0100

    Refactor

--- electron/window.ts
+++ electron/window.ts
@@ -8,6 +8,7 @@ import installExtension, {
 import path from 'path'
 import debug from 'electron-debug'
 
+// todo: menu — include shift in zoom accelerators
 export const createWindow = async (): Promise<BrowserWindow> => {
   const window = new BrowserWindow({
     width: 1200,
@@ -39,21 +40,21 @@ export const createWindow = async (): Promise<BrowserWindow> => {
     shell.openExternal(url)
   })
 
-  if (isDev) {
-    debug()
+  window.webContents.on('did-frame-finish-load', async () => {
+    if (isDev) {
+      debug()
 
-    window.webContents.openDevTools()
-    window.webContents.on('did-frame-finish-load', async () => {
+      window.webContents.openDevTools()
       try {
         // doesn't on >=9 work: https://github.com/electron/electron/issues/23662
-        // this is the only reason we're using electron 8
+        // this is the only reason for using electron 8
         await installExtension(REACT_DEVELOPER_TOOLS)
         await installExtension(REDUX_DEVTOOLS)
       } catch (error) {
         console.error(error)
       }
-    })
-  }
+    }
+  })
 
   return window
 }
