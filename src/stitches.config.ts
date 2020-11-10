7e17fec65570f8cd686427b530a69b0f45177f84
martonlanga <martonlanga@gmail.com>
2020-11-10 11:46:24 +0100

    Change fonts

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -122,9 +122,9 @@ export const theme = {
   },
   fonts: {
     $sans:
-      '"Helvetica", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      'Inter, "Helvetica", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
     $mono:
-      '"Courier New", Courier, Menlo, Monaco, Consolas, "Liberation Mono", monospace',
+      '"Source Code Pro", "Courier New", Courier, Menlo, Monaco, Consolas, "Liberation Mono", monospace',
     $serif: 'Georgia, Cambria, "Times New Roman", Times, serif',
   },
   space: {
