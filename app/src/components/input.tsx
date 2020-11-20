fe3d23a1922c5a3092636fa73c0d3cc3f171ca12
martonlanga <martonlanga@gmail.com>
2020-11-20 13:38:48 +0100

    Refactor suggestions

--- app/src/components/input.tsx
+++ app/src/components/input.tsx
@@ -1,19 +1,13 @@
-import { formatDistanceToNow } from 'date-fns'
-import Markdown from 'markdown-to-jsx'
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
-import { Portal } from 'react-portal'
-import { createEditor, Editor, Node, Range, Transforms } from 'slate'
+import React, { useMemo, useRef } from 'react'
+import { useDebounce } from 'react-use'
+import { createEditor, Editor, Node, Transforms } from 'slate'
 import { withHistory } from 'slate-history'
 import { Editable, ReactEditor, Slate, withReact } from 'slate-react'
-import type { CellTypeWithFocused, Message, Suggestion } from '../../types'
-import { ipc, useListener } from '../lib'
-import { styled } from '../stitches.config'
+import type { CellTypeWithFocused, Message } from '../../types'
+import { ipc } from '../lib'
 import useStore from '../store'
 import { Div } from './shared'
-import { Dir } from './svg'
-import { useDebounce } from 'react-use'
-
-// todo: refactor
+import Suggestions from './suggestions'
 
 if (import.meta.hot) {
   // slate not happy w/ hot reload
@@ -27,27 +21,12 @@ const Input: React.FC<CellTypeWithFocused> = ({
   focused,
 }) => {
   const dispatch = useStore(state => state.dispatch)
-  const theme = useStore(state => state.theme)
   const editor = useMemo(() => withHistory(withReact(createEditor())), [])
-  const [val, setVal] = useState([
-    {
-      type: 'paragraph',
-      children: [{ text: '' }],
-    },
-  ] as Node[])
-
-  const suggestionRef = useRef<HTMLDivElement>(null)
   const inputRef = useRef<HTMLDivElement>(null)
 
   const input = useMemo(() => value.map(n => Node.string(n)).join('\n'), [
     value,
   ])
-
-  const [suggestions, setSuggestions] = useState<Suggestion[] | null>([])
-  const [index, setIndex] = useState(0)
-  const [direction, setDirection] = useState<'column' | 'column-reverse'>(
-    'column',
-  )
 
   // const renderElement = useCallback(props => <Element {...props} />, [])
 
@@ -61,278 +40,75 @@ const Input: React.FC<CellTypeWithFocused> = ({
         currentDir,
       } as Message)
     },
-    20,
+    100,
     [id, input, currentDir],
   )
 
-  useListener(`suggestions-${id}`, (_, suggestions) =>
-    setSuggestions(suggestions),
-  )
-
-  console.log(suggestions)
-
-  // update suggestions box position
-  useEffect(() => {
-    const { selection } = editor
-    if (suggestionRef.current && inputRef.current && selection) {
-      const suggestionElement = suggestionRef.current
-      const inputElement = inputRef.current
-
-      const [start] = Range.edges(selection)
-      const before = Editor.before(editor, start, { unit: 'character' })
-      const beforeRange = before && Editor.range(editor, before, start)
-      if (!beforeRange) return
-
-      const domRange = ReactEditor.toDOMRange(editor, beforeRange)
-      const rect = domRange.getBoundingClientRect()
-
-      // calculate available top/bottom space of the element
-      const topSpace = inputElement.offsetTop + inputElement.offsetHeight
-      const bottomSpace = window.innerHeight - inputElement.offsetTop - 100
-
-      setDirection(bottomSpace > topSpace ? 'column' : 'column-reverse')
-
-      // todo: broken again w/ new layout
-      // calculate position of the suggestions box
-      const top =
-        bottomSpace > topSpace
-          ? rect.top + rect.height
-          : rect.top - suggestionElement.offsetHeight - 10
-
-      // update values
-      suggestionElement.style.top = `${top}px`
-      suggestionElement.style.left = `${rect.left}px`
-    }
-  }, [editor, index, suggestions])
-
-  const onKeyDown = useCallback(
-    (event: React.KeyboardEvent<HTMLDivElement>) => {
-      if (suggestions && suggestions.length > 0) {
-        // focused on suggestions
-        switch (event.key) {
-          case 'ArrowUp': {
-            event.preventDefault()
-            event.stopPropagation()
-
-            const nextIndex =
-              direction !== 'column-reverse'
-                ? index <= 0
-                  ? suggestions.length - 1
-                  : index - 1
-                : index >= suggestions.length - 1
-                ? 0
-                : index + 1
-            setIndex(nextIndex)
-            break
-          }
-          case 'ArrowDown': {
-            event.preventDefault()
-            event.stopPropagation()
-
-            const nextIndex =
-              direction === 'column-reverse'
-                ? index <= 0
-                  ? suggestions.length - 1
-                  : index - 1
-                : index >= suggestions.length - 1
-                ? 0
-                : index + 1
-            setIndex(nextIndex)
-            break
-          }
-          case 'Tab':
-            // expand suggestion
-            event.preventDefault()
-            insertSuggestion(editor, suggestions[index].command)
-
-            setSuggestions(null)
-            break
-          case 'Escape':
-            event.preventDefault()
-            setSuggestions(null)
-            break
-        }
-      }
-
-      if (event.key === 'Enter') {
-        // run cell
-        event.preventDefault() // don't allow multiline input
-
-        const suggestion =
-          suggestions && suggestions[index] && suggestions[index].command
-        if (typeof suggestion === 'string') {
-          insertSuggestion(editor, suggestion)
-          dispatch({
-            type: 'run-cell',
-            id,
-            input: suggestion,
-          })
-
-          // todo: doesn't work again... maybe blur would fix it
-          setSuggestions(() => null)
-        } else {
-          dispatch({ type: 'run-cell', id, input })
-        }
-      }
-    },
-    [suggestions, index, editor, id, input, direction, dispatch],
-  )
-
   return (
-    <Div
-      ref={inputRef}
-      css={{
-        width: '100%',
-        py: '$2',
-        position: 'relative',
-        fontWeight: '$semibold',
-        letterSpacing: '$snug',
-        fontSize: '$base',
-      }}
-    >
-      <Slate
-        editor={editor}
-        value={val}
-        onChange={value => {
-          // console.log('val', value)
-          setVal(value)
-          dispatch({
-            type: 'set-cell',
-            id,
-            cell: { value },
-          })
+    <>
+      <Div
+        ref={inputRef}
+        css={{
+          width: '100%',
+          py: '$2',
+          position: 'relative',
+          fontWeight: '$semibold',
+          letterSpacing: '$snug',
+          fontSize: '$base',
         }}
       >
-        <Editable
-          autoFocus
-          placeholder=">"
-          onKeyDown={onKeyDown}
-          // renderElement={renderElement}
-          onFocus={() => focusInput(editor)}
-        />
-        {suggestions && (
-          <Portal>
-            <Div
-              ref={suggestionRef}
-              css={{
-                position: 'absolute',
+        <Slate
+          editor={editor}
+          value={value}
+          onChange={value => {
+            // console.log('val', value)
+            dispatch({
+              type: 'set-cell',
+              id,
+              cell: { value },
+            })
+          }}
+        >
+          <Editable
+            autoFocus
+            placeholder=">"
+            onFocus={() => {
+              focusInput(editor)
+            }}
+            onKeyDown={event => {
+              if (event.key === 'Enter') {
+                // run cell
+                event.preventDefault() // don't allow multiline input
+                dispatch({ type: 'run-cell', id, input })
 
-                // important to set these to avoid flash
-                top: '-99999px',
-                left: '-99999px',
+                // const suggestion =
+                //   suggestions && suggestions[index] && suggestions[index].command
+                // if (typeof suggestion === 'string') {
+                //   insertSuggestion(editor, suggestion)
+                //   dispatch({
+                //     type: 'run-cell',
+                //     id,
+                //     input: suggestion,
+                //   })
 
-                zIndex: 1,
-                display: 'flex',
-                flexDirection: direction,
-                backgroundColor: theme.colors.$focusedBackgroundColor,
-                borderRadius: '$default',
-                boxShadow: '$3xl',
-                maxHeight: '60vh',
-                border: '1px solid $accentColor',
-              }}
-            >
-              {suggestions.map((suggestion, i) => (
-                <SuggestionItem
-                  key={i}
-                  type={suggestion.kind}
-                  state={i === index ? 'focused' : 'default'}
-                >
-                  {suggestion.kind === 'directory' && (
-                    <Dir css={{ width: '$3', height: '100%', mr: '$1' }} />
-                  )}
-                  {suggestion.display
-                    .split('')
-                    .map((c, i) =>
-                      suggestion.indexes.includes(BigInt(i)) ? (
-                        <b key={i}>{c}</b>
-                      ) : (
-                        <span key={i}>{c}</span>
-                      ),
-                    )}
-                  {suggestion.date && (
-                    <Div
-                      css={{
-                        ml: 'auto',
-                        pl: '$1',
-                        color: i === index ? '$gray200' : '$gray600',
-                        fontSize: '$xs',
-                      }}
-                    >
-                      {formatDistanceToNow(parseInt(suggestion.date))}
-                    </Div>
-                  )}
-                  {suggestion.documentation && (
-                    <Div
-                      css={{
-                        position: 'absolute',
-                        left: '100%',
-                        top: 0,
-                        color: '$primaryTextColor',
-                        backgroundColor: '$backgroundColor',
-                        px: '$2',
-                        borderRadius: '$default',
-                        boxShadow: '$3xl',
-                        zIndex: 1,
-                        overflowY: 'auto',
-                        minWidth: '300px',
-                        maxHeight: '60vh',
-                        fontSize: '$xs',
-
-                        '* > *': {
-                          my: '$2',
-                        },
-                      }}
-                    >
-                      <Markdown>{suggestion.documentation}</Markdown>
-                    </Div>
-                  )}
-                </SuggestionItem>
-              ))}
-            </Div>
-          </Portal>
-        )}
-      </Slate>
-    </Div>
+                //   // todo: doesn't work again... maybe blur would fix it
+                //   setSuggestions(() => null)
+                // } else {
+                // }
+              }
+            }}
+            // renderElement={renderElement}
+          />
+        </Slate>
+      </Div>
+      <Suggestions
+        id={id}
+        editor={editor}
+        inputRef={inputRef}
+        focused={focused}
+      />
+    </>
   )
-}
-
-const SuggestionItem = styled(Div, {
-  p: '$2',
-  display: 'flex',
-  alignItems: 'center',
-  flexWrap: 'nowrap',
-  fontSize: '$sm',
-
-  '& + &': {
-    borderTop: '1px solid $accentColor',
-  },
-
-  variants: {
-    type: {
-      directory: {},
-      bash: {},
-      executable: {},
-    },
-    state: {
-      focused: {
-        backgroundColor: '$selectedSuggestionBackgroundColor',
-        color: '$selectedSuggestionColor',
-      },
-      default: {
-        color: '$secondaryTextColor',
-      },
-    },
-  },
-})
-
-const insertSuggestion = (editor: Editor, input: string) => {
-  const all = Editor.range(
-    editor,
-    Editor.start(editor, []),
-    Editor.end(editor, []),
-  )
-  Transforms.select(editor, all)
-  Transforms.insertText(editor, input)
 }
 
 const focusInput = (editor: ReactEditor) => {
---
+++ app/src/components/suggestions.tsx
@@ -0,0 +1,313 @@
+import { CounterClockwiseClockIcon, GearIcon } from '@modulz/radix-icons'
+import { formatDistanceToNow } from 'date-fns'
+import Markdown from 'markdown-to-jsx'
+import React, { useEffect, useRef, useState } from 'react'
+import { useKey } from 'react-use'
+import { Editor, Range, Transforms } from 'slate'
+import { ReactEditor } from 'slate-react'
+import type { Suggestion } from 'types/shared'
+import { useListener } from '../lib'
+import { styled } from '../stitches.config'
+import { Div, Span } from './shared'
+import { Folder } from './svg'
+
+interface Props {
+  id: string
+  editor: Editor & ReactEditor
+  inputRef: React.RefObject<HTMLDivElement>
+  focused: boolean
+}
+
+const Suggestions: React.FC<Props> = ({ id, editor, inputRef, focused }) => {
+  const suggestionRef = useRef<HTMLDivElement>(null)
+
+  const [show, setShow] = useState(true)
+  const [suggestions, setSuggestions] = useState<Suggestion[] | null>([])
+  const [index, setIndex] = useState(0)
+  const [direction, setDirection] = useState<'column' | 'column-reverse'>(
+    'column',
+  )
+
+  useListener(
+    `suggestions-${id}`,
+    (_, suggestions) => (show ? setSuggestions(suggestions) : setShow(true)),
+    [show],
+  )
+
+  // update popup position
+  useEffect(() => {
+    const { selection } = editor
+    const suggestionElement = suggestionRef.current
+    const inputElement = inputRef.current
+
+    if (selection && suggestionElement && inputElement) {
+      const [start] = Range.edges(selection)
+      const before = Editor.before(editor, start, { unit: 'character' })
+      const beforeRange = before && Editor.range(editor, before, start)
+      if (!beforeRange) return
+
+      const domRange = ReactEditor.toDOMRange(editor, beforeRange)
+      const rect = domRange.getBoundingClientRect()
+      const inputRect = inputElement.getBoundingClientRect()
+
+      const topSpace = inputRect.y
+      const bottomSpace = window.innerHeight - inputRect.y - inputRect.height
+
+      setDirection(bottomSpace > topSpace ? 'column' : 'column-reverse')
+
+      if (suggestionElement.children[0])
+        // set items maxHeight
+        (suggestionElement.children[0] as HTMLDivElement).style.maxHeight = `${
+          (topSpace > bottomSpace ? topSpace : bottomSpace) - 10
+        }px`
+      const top =
+        topSpace > bottomSpace
+          ? inputRect.top - suggestionElement.offsetHeight
+          : rect.bottom
+
+      // update values
+      suggestionElement.style.top = `${top}px`
+      suggestionElement.style.left = `${rect.left}px`
+    }
+  }, [editor, index, suggestions])
+
+  // scroll item into view
+  useEffect(() => {
+    if (suggestionRef.current && suggestionRef.current.children[0]) {
+      const item = suggestionRef.current.children[0].children[
+        index
+      ] as HTMLDivElement
+      if (item) item.scrollIntoView()
+    }
+  }, [index])
+
+  // can't use onKeyDown, because Slate captures all events
+  useKey(
+    () => true,
+    event => {
+      if (focused && suggestions && suggestions.length > 0) {
+        // focused on suggestions
+        switch (event.key) {
+          case 'ArrowUp': {
+            event.preventDefault()
+
+            const nextIndex =
+              direction !== 'column-reverse'
+                ? index <= 0
+                  ? suggestions.length - 1
+                  : index - 1
+                : index >= suggestions.length - 1
+                ? 0
+                : index + 1
+            setIndex(nextIndex)
+            break
+          }
+          case 'ArrowDown': {
+            event.preventDefault()
+
+            const nextIndex =
+              direction === 'column-reverse'
+                ? index <= 0
+                  ? suggestions.length - 1
+                  : index - 1
+                : index >= suggestions.length - 1
+                ? 0
+                : index + 1
+            setIndex(nextIndex)
+            break
+          }
+          case 'Tab':
+          case 'Enter':
+            event.preventDefault()
+            insertSuggestion(editor, suggestions[index].command)
+            setShow(false)
+            break
+          case 'Escape':
+            event.preventDefault()
+            setSuggestions(null)
+            break
+        }
+      }
+    },
+    {},
+    [focused, suggestions, editor, index, direction],
+  )
+
+  if (!suggestions || suggestions.length < 1 || !focused || !show) return null
+
+  return (
+    <Popup ref={suggestionRef}>
+      <Items
+        style={{
+          flexDirection: direction,
+        }}
+      >
+        {suggestions.map((suggestion, i) => (
+          <Item
+            key={i}
+            type={suggestion.kind}
+            state={i === index ? 'focused' : 'default'}
+            onClick={() => {
+              insertSuggestion(editor, suggestions[i].command)
+              setShow(false)
+            }}
+          >
+            <Span
+              css={{
+                mr: '$1',
+
+                display: 'flex',
+                alignItems: 'center',
+
+                svg: {
+                  width: '$3',
+                },
+              }}
+            >
+              {renderIcon(suggestion.kind)}
+            </Span>
+
+            <Span css={{ whiteSpace: 'pre', overflow: 'hidden' }}>
+              {renderSuggestionText(suggestion.display, suggestion.indexes)}
+            </Span>
+
+            {suggestion.date && (
+              <Date>{formatDistanceToNow(parseInt(suggestion.date))} ago</Date>
+            )}
+          </Item>
+        ))}
+      </Items>
+      {suggestions[index] && suggestions[index].documentation && (
+        <DocumentationPopup>
+          {/* @ts-ignore */}
+          <Markdown>{suggestions[index].documentation}</Markdown>
+        </DocumentationPopup>
+      )}
+    </Popup>
+  )
+}
+
+export default Suggestions
+
+const renderIcon = (kind: Suggestion['kind']) => {
+  switch (kind) {
+    case 'directory': {
+      return <Folder />
+    }
+    case 'executable': {
+      return <GearIcon />
+    }
+    case 'bash': {
+      return <CounterClockwiseClockIcon />
+    }
+    default:
+      return null
+  }
+}
+
+const renderSuggestionText = (text: string, indexes: bigint[]) => {
+  const children = []
+  for (let i = 0; i < text.length; i++) {
+    children[i] = indexes.includes(BigInt(i)) ? (
+      <Span key={i} css={{ fontWeight: '$bold' }}>
+        {text[i]}
+      </Span>
+    ) : (
+      <span key={i}>{text[i]}</span>
+    )
+  }
+  return children
+}
+
+const insertSuggestion = (editor: Editor, input: string) => {
+  const all = Editor.range(
+    editor,
+    Editor.start(editor, []),
+    Editor.end(editor, []),
+  )
+  Transforms.select(editor, all)
+  Transforms.insertText(editor, input)
+}
+
+const Popup = styled(Div, {
+  position: 'fixed',
+  // important to set these to avoid flash
+  top: '-99999px',
+  left: '-99999px',
+  zIndex: 1,
+})
+
+const Items = styled(Div, {
+  display: 'flex',
+  overflowY: 'auto',
+  overflowX: 'hidden',
+  maxWidth: '22rem',
+  borderRadius: '$default',
+  backgroundColor: '$focusedBackgroundColor',
+  boxShadow: '$3xl',
+})
+
+const Item = styled(Div, {
+  p: '$2',
+  display: 'flex',
+  alignItems: 'center',
+  flexWrap: 'nowrap',
+  fontSize: '$sm',
+  cursor: 'pointer',
+
+  ':hover': {
+    backgroundColor: '$selectedSuggestionBackgroundColor',
+    color: '$selectedSuggestionColor',
+    opacity: 0.92,
+  },
+
+  variants: {
+    type: {
+      directory: {},
+      bash: {},
+      executable: {},
+    },
+    state: {
+      focused: {
+        backgroundColor: '$selectedSuggestionBackgroundColor',
+        color: '$selectedSuggestionColor',
+      },
+      default: {
+        color: '$secondaryTextColor',
+      },
+    },
+  },
+})
+
+const Date = styled(Span, {
+  ml: 'auto',
+  pl: '$1',
+  color: '$secondaryTextColor',
+  fontSize: '$xs',
+})
+
+const DocumentationPopup = styled(Div, {
+  position: 'absolute',
+  left: 'calc(100% + 3px)',
+  zIndex: 1,
+  top: 0,
+  width: '25rem',
+
+  // todo: calculate max w & h
+  maxWidth: '70vh',
+  maxHeight: '70vh',
+
+  color: '$primaryTextColor',
+  px: '$3',
+  py: '$2',
+  overflowY: 'auto',
+  fontSize: '.8em',
+  backgroundColor: '$focusedBackgroundColor',
+  border: '1px solid $accentColor',
+  borderRadius: '$default',
+  boxShadow: '2xl',
+
+  '* > *': {
+    my: '$3',
+  },
+})
