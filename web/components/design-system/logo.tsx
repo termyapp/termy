24602f9b6fc00a0d51c782d4e17c7b44c27f6246
martonlanga <martonlanga@gmail.com>
2020-11-23 02:21:19 +0100

    Add link to logo

--- web/components/design-system/logo.tsx
+++ web/components/design-system/logo.tsx
@@ -1,43 +1,48 @@
+import Link from 'next/link'
 import React from 'react'
 import { Text } from './text'
 
 export const Logo = () => {
   return (
-    <Text
-      css={{
-        fontFamily: 'Futura',
-        fontSize: '$6',
-        letterSpacing: -0.8,
-        fontWeight: 'bold',
-        background:
-          'linear-gradient(to right bottom, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, .9) 100%)',
-        WebkitBackgroundClip: 'text',
-        WebkitTextFillColor: 'transparent',
-        lineHeight: 1.3, // somehow it clips it if it's not heigh enough?!
-        position: 'relative',
-        textDecoration: 'none',
-      }}
-    >
-      Termy
+    <Link href="/">
       <Text
+        as="a"
         css={{
-          position: 'absolute',
-          top: -8,
-          left: 'calc(100% - 10px)',
-          WebkitTextFillColor: 'rgba(200, 10, 10, .8)',
-          backgroundColor: '$orange200',
-          px: '$1',
-          lineHeight: 1.7,
-          fontWeight: 400,
-          fontSize: '10px',
-          borderRadius: '$lg',
-          letterSpacing: '.4px',
-          pointerEvents: 'none',
-          zIndex: -1,
+          cursor: 'pointer',
+          fontFamily: 'Futura',
+          fontSize: '$6',
+          letterSpacing: -0.8,
+          fontWeight: 'bold',
+          background:
+            'linear-gradient(to right bottom, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, .9) 100%)',
+          WebkitBackgroundClip: 'text',
+          WebkitTextFillColor: 'transparent',
+          lineHeight: 1.3, // somehow it clips it if it's not heigh enough?!
+          position: 'relative',
+          textDecoration: 'none',
         }}
       >
-        Beta
+        Termy
+        <Text
+          css={{
+            position: 'absolute',
+            top: -8,
+            left: 'calc(100% - 10px)',
+            WebkitTextFillColor: 'rgba(200, 10, 10, .8)',
+            backgroundColor: '$orange200',
+            px: '$1',
+            lineHeight: 1.7,
+            fontWeight: 400,
+            fontSize: '10px',
+            borderRadius: '$lg',
+            letterSpacing: '.4px',
+            pointerEvents: 'none',
+            zIndex: -1,
+          }}
+        >
+          Beta
+        </Text>
       </Text>
-    </Text>
+    </Link>
   )
 }
