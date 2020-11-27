0a2cd3d901f3c76985f87a316aef310012feaa3d
agomila96@gmail.com <aitor2008>
2020-11-27 16:12:48 +0100

    For other OS' compatibility check if the current OS is MacOS

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -33,6 +33,8 @@ const createMainWindow = async () => {
   app.on('activate', async function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) await createWindow()
+    if(process.platform === "darwin") {
+      if (BrowserWindow.getAllWindows().length === 0) await createWindow()
+    }
   })
 }
