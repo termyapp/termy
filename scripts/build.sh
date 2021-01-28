f39b997a37cbba764e478d22b2a018d22e0ef1eb
martonlanga <martonlanga@gmail.com>
2021-01-28 07:51:53 +0100

    Add rust check & run electron build at the end

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,16 +2,24 @@
 
 echo Node: $(node -v) || echo Node not found
 echo Yarn: $(yarn -v) || echo Yarn not found
-# Rust check is missing
+echo Cargo: $(cargo version) || echo Cargo not found
 
-# Setup everything
 
+# Building
+
+echo Building native module...
 cd native || echo Failed to visit native. Current working directory: $(pwd). Are you in the root?
-yarn || echo Failed to install yarn dependencies. Is yarn installed? && yarn build
+yarn || echo Failed to install yarn dependencies. Is yarn installed? && yarn build-release
 
 cd ../electron
 
-yarn && yarn build-webpack & yarn build
+echo Bundling main...
+yarn && yarn build-webpack
 
+echo Building app...
 cd ../app
+yarn && yarn build
+
+echo Building electron...
+cd ../electron
 yarn && yarn build
