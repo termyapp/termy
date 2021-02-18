6a932dbf8ac481c16d41e659221f0f37fa823b19
martonlanga <martonlanga@gmail.com>
2021-02-18 10:59:27 +0100

    CrossPath

--- native/src/command/internal/view.rs
+++ native/src/command/internal/view.rs
@@ -2,33 +2,33 @@ use anyhow::Result;
 use std::{fs, path::Path};
 
 pub fn view<P: AsRef<Path>>(path: Option<P>) -> Result<String> {
-    if let Some(path) = path {
-        let extension = path.as_ref().extension().unwrap_or_default();
-        let content = fs::read_to_string(&path)?;
+  if let Some(path) = path {
+    let extension = path.as_ref().extension().unwrap_or_default();
+    let content = fs::read_to_string(&path)?;
 
-        return Ok(format!(
-            "```{}\n{}\n```",
-            extension.to_string_lossy(),
-            content
-        ));
-    } else {
-        let message = format!("Path does not exist");
-        return Ok(message);
-    }
+    return Ok(format!(
+      "```{}\n{}\n```",
+      extension.to_string_lossy(),
+      content
+    ));
+  } else {
+    let message = format!("Path does not exist");
+    return Ok(message);
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::util::paths::test_dir;
+  use crate::util::paths::test_dir;
 
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn reads_markdown_file() {
-        let readme_file = test_dir().unwrap().join("readme.md");
-        assert_eq!(
+  #[test]
+  fn reads_markdown_file() {
+    let readme_file = test_dir().unwrap().join("readme.md");
+    assert_eq!(
             view(Some(readme_file)).unwrap(),
-            "```md\nFiles and folders to test the internal commands.\n\n```".to_string()
+            "```md\nFiles and folders to test the internal commands.\n\nMake sure that the tests pass after modifying these files.\n\n```".to_string()
         );
-    }
+  }
 }
--- native/src/util/paths.rs
+++ native/src/util/paths.rs
@@ -1,5 +1,12 @@
 use anyhow::Result;
-use std::{env, path::PathBuf};
+use path_slash::PathBufExt;
+use path_slash::PathExt;
+use std::{
+  env,
+  ffi::OsStr,
+  fmt,
+  path::{Path, PathBuf},
+};
 
 /// Path to Termy's root directory
 ///
@@ -11,26 +18,58 @@ use std::{env, path::PathBuf};
 ///
 /// | Environment| Dev | Production |
 /// | --- | --- | --- |
-/// | MacOS| `termy/electron/native` | `` |
+/// | MacOS| `termy/electron/native` | Production |
 /// | Linux| Dev | Production |
 /// | Windows| Dev | Production |
 ///
 /// Path is different during build time, running Termy in development mode and running in production
 pub fn root_path() -> Result<PathBuf> {
-    let path = if cfg!(debug_assertions) {
-        env::current_dir()?
-    } else {
-        // https://www.electron.build/configuration/contents.html#filesetto
-        env::current_exe()?
-    };
-    Ok(path)
+  let path = if cfg!(debug_assertions) {
+    env::current_dir()?
+  } else {
+    // https://www.electron.build/configuration/contents.html#filesetto
+    env::current_exe()?
+  };
+  Ok(path)
 }
 
 pub fn test_dir() -> Result<PathBuf> {
-    Ok(root_path()?.join("test"))
+  Ok(root_path()?.join("test"))
+}
+
+pub fn home_dir() {
+  let home = dirs::home_dir().unwrap();
+}
+
+struct CrossPath {
+  path: PathBuf,
+}
+
+impl CrossPath {
+  pub fn new<S: AsRef<OsStr> + ?Sized>(s: &S) -> CrossPath {
+    let path = Path::new(s).to_owned();
+    CrossPath { path }
+  }
+
+  pub fn display(&self) -> String {
+    format!("{}", self)
+  }
+}
+
+impl fmt::Display for CrossPath {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{}", self.path.to_slash().unwrap())
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
+
+  #[test]
+  #[cfg(target_os = "windows")]
+  fn converts_backslash_to_forward_slash() {
+    let cross_path = CrossPath::new(r"C:\foo\bar.txt");
+    assert_eq!("C:/foo/bar.txt", &(cross_path.display()));
+  }
 }
