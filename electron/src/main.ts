b09129dddea41170bbbf05c819e62dd424dc2b4a
martonlanga <martonlanga@gmail.com>
2021-02-18 11:26:43 +0100

    Disable refresh using localshortcuts

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -3,6 +3,7 @@ import contextMenu from 'electron-context-menu'
 import fixPath from 'fix-path'
 import setupMessages from './messages'
 import { createWindow } from './window'
+import electronLocalshortcut from 'electron-localshortcut'
 
 // fixes: https://github.com/termyapp/termy/issues/8
 fixPath()
@@ -20,17 +21,6 @@ let mainWindow: BrowserWindow
 app.on('ready', () => {
   createMainWindow()
   setupMessages()
-
-  // prevent window from closing (until we don't have a proper menu window)
-  const menu = Menu.getApplicationMenu()
-  if (menu) {
-    // @ts-ignore
-    const fileMenu = menu.items.find(item => item.role === 'filemenu')
-    if (fileMenu) {
-      fileMenu.visible = false
-      Menu.setApplicationMenu(menu)
-    }
-  }
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
@@ -45,10 +35,28 @@ app.on('window-all-closed', () => {
 
 const createMainWindow = async () => {
   mainWindow = await createWindow()
+  disableRefresh(mainWindow)
 
   app.on('activate', async function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) await createWindow()
+  })
+
+  return mainWindow
+}
+
+// until we don't have a custom menu
+const disableRefresh = (window: BrowserWindow) => {
+  window.on('focus', () => {
+    electronLocalshortcut.register(
+      window,
+      ['CommandOrControl+R', 'CommandOrControl+Shift+R', 'F5'],
+      () => {},
+    )
+  })
+
+  window.on('blur', () => {
+    electronLocalshortcut.unregisterAll(window)
   })
 }
