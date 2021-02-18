9294b983cab561acce0be9f5b01d48e485da90e5
martonlanga <martonlanga@gmail.com>
2021-02-18 10:10:46 +0100

    Update submodules

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 # Prerequirements
-
 echo Node: "$(node -v)" || echo Node not found
 echo Yarn: "$(yarn -v)" || echo Yarn not found
 echo Cargo: "$(cargo version)" || echo Cargo not found
 
+# Update submodules
+echo Updating submodules...
+git submodule update --recursive
 
 # Building
-
 echo Building native module...
 cd native || echo Failed to visit native. Current working directory: "$(pwd)". Are you in the root?
 yarn || echo Failed to install yarn dependencies. Is yarn installed? && yarn build-release
