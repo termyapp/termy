726d4a8f622e67dfb75c7f49ce3ae894c2678cbb
martonlanga <martonlanga@gmail.com>
2021-02-20 11:45:02 +0100

    fix(shortcut): run suggestion if it matches input

--- app/src/termy/input.tsx
+++ app/src/termy/input.tsx
@@ -85,10 +85,17 @@ export default function Input({
 
                 // Contexts:
                 // https://code.visualstudio.com/docs/getstarted/keybindings#_available-contexts
+
                 editor.addCommand(
                   KeyCode.Enter,
                   () => {
+                    const before = editor.getModel()?.getValue()
                     editor.trigger('', 'acceptSelectedSuggestion', {})
+                    const after = editor.getModel()?.getValue()
+                    if (typeof before === 'string' && before === after) {
+                      // suggestion was same as the input value, so let's run it!
+                      dispatch({ type: 'run-cell', id })
+                    }
                   },
                   'suggestWidgetVisible',
                 )
