103985e440d176a749ac255acff77e6c6a9e10ef
martonlanga <martonlanga@gmail.com>
2021-02-23 10:41:25 +0100

    feat(ui): add new-tab icon and update tab name

--- app/src/termy/nav.tsx
+++ app/src/termy/nav.tsx
@@ -1,4 +1,4 @@
-import { Div, Flex } from '@components'
+import { Div, Flex, Svg } from '@components'
 import useStore from '@src/store'
 import { ipc, isMac } from '@src/utils'
 import type { WindowMessage } from '@types'
@@ -38,18 +38,43 @@ export default function Nav({ tabs, activeTab }: Props) {
                 backgroundColor:
                   id === activeTab ? '$focusedBackground' : '$background',
                 px: '$3',
-                color: '$secondaryForeground',
+                color: '$foreground',
                 fontSize: '$xs',
                 borderRadius: '$sm',
-                fontWeight: '$light',
+                fontWeight: '$medium',
+                opacity: id === activeTab ? 1 : 0.5,
               }}
               onClick={() => {
                 dispatch({ type: 'focus-tab', id })
               }}
             >
-              ⌘{i + 1}
+              {i + 1}
             </Flex>
           ))}
+      </Flex>
+
+      <Flex
+        onClick={() => dispatch({ type: 'new-tab' })}
+        css={{
+          cursor: 'pointer',
+          width: '15px',
+          height: '15px',
+          mr: '$3',
+          opacity: 0.6,
+
+          ':hover': {
+            opacity: 1,
+          },
+        }}
+      >
+        <Svg viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M8 2.75C8 2.47386 7.77614 2.25 7.5 2.25C7.22386 2.25 7 2.47386 7 2.75V7H2.75C2.47386 7 2.25 7.22386 2.25 7.5C2.25 7.77614 2.47386 8 2.75 8H7V12.25C7 12.5261 7.22386 12.75 7.5 12.75C7.77614 12.75 8 12.5261 8 12.25V8H12.25C12.5261 8 12.75 7.77614 12.75 7.5C12.75 7.22386 12.5261 7 12.25 7H8V2.75Z"
+            fill="currentColor"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+          ></path>
+        </Svg>
       </Flex>
 
       {!isMac && (
