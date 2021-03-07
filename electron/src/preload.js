a03ecd65c6b05c9b9373652123fe7837f7614fe0
martonlanga <martonlanga@gmail.com>
2021-03-07 10:44:35 +0100

    perf: add ipc time measures

--- electron/src/preload.js
+++ electron/src/preload.js
@@ -1,8 +1,19 @@
 const { ipcRenderer, contextBridge } = require('electron')
 
+const INVOKE = 'INVOKE'
+const INVOKE_SYNC = 'INVOKE_SYNC'
+
 contextBridge.exposeInMainWorld('ipc', {
-  invoke: (...args) => ipcRenderer.invoke('ipc', ...args),
-  sync: (...args) => ipcRenderer.sendSync('ipc-sync', ...args),
+  invoke: (...args) => {
+    console.time(INVOKE)
+    ipcRenderer.invoke('ipc', ...args)
+    console.timeEnd(INVOKE)
+  },
+  sync: (...args) => {
+    console.time(INVOKE_SYNC)
+    ipcRenderer.sendSync('ipc-sync', ...args)
+    console.timeEnd(INVOKE_SYNC)
+  },
   on: (...args) => ipcRenderer.on(...args),
   removeAllListeners: (...args) => ipcRenderer.removeAllListeners(...args),
 })
