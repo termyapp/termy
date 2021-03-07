4e2f99dade4a2d4d829bdb93f9af93c801b3ed90
martonlanga <martonlanga@gmail.com>
2021-03-07 12:30:33 +0100

    fix: return results

--- electron/src/preload.js
+++ electron/src/preload.js
@@ -6,13 +6,15 @@ const INVOKE_SYNC = 'INVOKE_SYNC'
 contextBridge.exposeInMainWorld('ipc', {
   invoke: (...args) => {
     console.time(INVOKE)
-    ipcRenderer.invoke('ipc', ...args)
+    const result = ipcRenderer.invoke('ipc', ...args)
     console.timeEnd(INVOKE)
+    return result
   },
   sync: (...args) => {
     console.time(INVOKE_SYNC)
-    ipcRenderer.sendSync('ipc-sync', ...args)
+    const result = ipcRenderer.sendSync('ipc-sync', ...args)
     console.timeEnd(INVOKE_SYNC)
+    return result
   },
   on: (...args) => ipcRenderer.on(...args),
   removeAllListeners: (...args) => ipcRenderer.removeAllListeners(...args),
