dc9d97cd10ea79baea0a5b61bab5a483251a3918
martonlanga <martonlanga@gmail.com>
2021-03-11 09:58:07 +0100

    feat: set up prod file logger

--- native/src/logger.rs
+++ native/src/logger.rs
@@ -1,32 +1,43 @@
+use crate::util::dirs;
 use anyhow::Result;
 use log::LevelFilter;
-use log4rs::append::console::ConsoleAppender;
+use log4rs::append::{console::ConsoleAppender, file::FileAppender};
 use log4rs::config::{Appender, Config, Root};
 use log4rs::encode::pattern::PatternEncoder;
+use std::fs::File;
+
+// patterns: https://docs.rs/log4rs/1.0.0/log4rs/encode/pattern/index.html
 
 pub fn init() -> Result<()> {
-  // File::create_new()
-  // todo: create ~/.termy folder and put log inside there & delete it on each init
-  // or .config folder (standard?)
-  // let file = FileAppender::builder()
-  //   .encoder(Box::new(PatternEncoder::new(
-  //     "{d(%Y-%m-%d %H:%M:%S)} {l} - {m}{n}",
-  //   ))) // https://docs.rs/log4rs/0.13.0/log4rs/encode/pattern/index.html
-  //   .build(dirs::home_dir().unwrap().to_string_lossy().to_string() + "/.termy.log")?;
+  let config = if cfg!(debug_assertions) {
+    let console = ConsoleAppender::builder()
+      .encoder(Box::new(PatternEncoder::new("{h({l})} | {m}{n}"))) // concurrently removes highlighting...
+      .build();
 
-  let console = ConsoleAppender::builder()
-    .encoder(Box::new(PatternEncoder::new("{h({l})} - {m}{n}"))) // concurrently removes highlighting...
-    .build();
+    Config::builder()
+      .appender(Appender::builder().build("console", Box::new(console)))
+      .build(
+        Root::builder()
+          .appender("console")
+          .build(LevelFilter::Trace),
+      )?
+  } else {
+    // get log file path (~/.termy/log)
+    let log_path = dirs::config().join("log");
 
-  let config = Config::builder()
-    // .appender(Appender::builder().build("logfile", Box::new(file)))
-    .appender(Appender::builder().build("console", Box::new(console)))
-    .build(
-      Root::builder()
-        // .appender("logfile")
-        .appender("console")
-        .build(LevelFilter::Trace),
-    )?;
+    // create a new file on each init
+    File::create(&log_path)?;
+
+    let file = FileAppender::builder()
+      .encoder(Box::new(PatternEncoder::new(
+        "{d(%H:%M:%S)} | {h({l})} | {m}{n}",
+      )))
+      .build(log_path)?;
+
+    Config::builder()
+      .appender(Appender::builder().build("file", Box::new(file)))
+      .build(Root::builder().appender("file").build(LevelFilter::Trace))?
+  };
 
   log4rs::init_config(config)?;
 
