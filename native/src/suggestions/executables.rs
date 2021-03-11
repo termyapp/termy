11cae2af75d84bf597f8ac075bdd3c6104f6799f
martonlanga <martonlanga@gmail.com>
2021-03-11 11:54:08 +0100

    perf: use hashmap
    dev tldr docs work

--- native/src/suggestions/executables.rs
+++ native/src/suggestions/executables.rs
@@ -1,27 +1,22 @@
-use std::{
-  fs::{self},
-  path::Path,
-};
-
 use super::{Priority, ProviderState, Suggestion, SuggestionProvider, SuggestionType};
+use crate::util::dirs::root_path;
 use anyhow::Result;
 use fuzzy_matcher::FuzzyMatcher;
 use is_executable::IsExecutable;
 use log::info;
+use std::{
+  collections::HashMap,
+  fs::{self},
+  path::Path,
+};
 
 lazy_static! {
   // todo: update this periodically
-  // todo: use hashmap, since lookups are faster w/ 15 or more elements
   pub static ref EXECUTABLES: Executables = Executables::new();
 }
 
-struct Executable {
-  name: String,
-  documentation: Option<String>,
-}
-
 pub struct Executables {
-  executables: Vec<Executable>,
+  executables: HashMap<String, Option<String>>, // name, documentation
 }
 
 impl Executables {
@@ -30,17 +25,14 @@ impl Executables {
     let paths: Vec<_> = std::env::split_paths(&path_var).collect();
 
     // todo: add `cmd` built-ins on windows
-    let mut executables = vec![];
+    let mut executables = HashMap::new();
     for path in paths {
       if let Ok(mut contents) = std::fs::read_dir(path) {
         while let Some(Ok(item)) = contents.next() {
           let mut path = item.path();
           if path.is_executable() {
-            #[cfg(windows)]
-            {
-              // PING.EXE -> PING
-              path.set_extension("");
-            }
+            // PING.EXE -> PING
+            path.set_extension("");
 
             let name = path
               .file_name()
@@ -49,15 +41,13 @@ impl Executables {
               .unwrap()
               .to_string();
 
-            let executable = Executable {
-              name: if cfg!(windows) {
-                name.to_lowercase()
-              } else {
-                name.to_owned()
-              },
-              documentation: tldr_docs(&name),
+            let key = if cfg!(windows) {
+              name.to_lowercase()
+            } else {
+              name.to_owned()
             };
-            executables.push(executable);
+
+            executables.insert(key, tldr_docs(&name));
           }
         }
       }
@@ -67,38 +57,30 @@ impl Executables {
   }
 
   pub fn contains(&self, name: &str) -> bool {
-    if self.executables.contains(&name)
-      || (cfg!(windows) && self.executables.contains(&name.to_lowercase()))
-    {
-      true
-    } else {
-      false
-    }
+    self.executables.contains_key(name)
+      // commands on windows are not case sensitive
+      || (cfg!(windows) && self.executables.contains_key(&name.to_lowercase()))
   }
 }
 
 impl SuggestionProvider for Executables {
   fn suggestions(&self, state: &mut ProviderState) -> Result<()> {
-    let executables = EXECUTABLES.executables.into_iter();
-    for Executable {
-      name,
-      documentation,
-    } in executables
-    {
+    let executables = self.executables.iter();
+    for (name, documentation) in executables {
       if let Some((score, _)) = state.matcher.fuzzy_indices(&name, state.value.as_ref()) {
         state.insert(
           name.clone(),
           Suggestion {
             label: name.clone(),
             insert_text: None,
-            score: if &(state.value) == &name {
+            score: if &state.value == name {
               // boosting to make sure exact matches are included
               score + Priority::High as i64
             } else {
               score
             },
             kind: SuggestionType::Executable,
-            documentation,
+            documentation: documentation.clone(),
             date: None,
           },
         );
@@ -110,14 +92,18 @@ impl SuggestionProvider for Executables {
 }
 
 fn tldr_docs(name: &str) -> Option<String> {
-  let path = if cfg!(debug_assertions) {
-    Path::new("../../external/tldr/pages/common").join(name)
+  let tldr_path = if cfg!(debug_assertions) {
+    root_path().join("../external/tldr/pages/common")
   } else {
-    Path::new("../external/tldr/common").join(name)
+    root_path().join("../external/tldr/common").join(name)
   };
+
+  let path = tldr_path.join(name.to_lowercase() + ".md");
+
+  info!("TDLR {:?}", path);
+
   if let Ok(contents) = fs::read_to_string(path) {
     info!("Found TLDR docs for {}", name);
-
     return Some(contents);
   }
 
@@ -132,10 +118,10 @@ mod tests {
   #[test]
   fn getting_executables() {
     let start = Instant::now();
-    let executables = EXECUTABLES.executables;
+    let executables = EXECUTABLES.executables.keys();
     let duration = start.elapsed();
 
-    println!("Time elapsed in expensive_function() is: {:?}", duration);
+    println!("Time elapsed: {:?}", duration);
     assert!(executables.len() > 1);
   }
 }
